7c2ef494addc5495a372d5cfb0ede26aa88a8c36
jenad3110 <jenad.mo@gmail.com>
2024-11-13 10:46:11 -0500

    Merge remote-tracking branch 'origin/master'

--- src/test/java/com/tutorialsNinja/qa/HomePageM.java
+++ src/test/java/com/tutorialsNinja/qa/HomePageM.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-
 public class HomePageM extends CommonApiTest {
 
 @Test
